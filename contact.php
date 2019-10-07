78ed5c6e11a28bd724f07c2e82b3e655666b810b
gurnihal52 <46015716+gurnihal52@users.noreply.github.com>
2019-10-07 17:43:09 +1000

    view editing

--- contact.php
+++ contact.php
@@ -3,12 +3,16 @@
 				
 			
 	<div class="col-md-12 " style="margin-top:0px;">
-		<div class="col-md-10 col-md-offset-1 feed" style="margin-bottom:0px; ">
-			<h3>Contact Us</h3>
-			<hr>
-			<p><i class="fa fa-phone"></i> +00-00000000</p>
-			<p><i class="fa fa-envelope"></i> test@test.com</p>
+		
+			<div class="col-md-10 col-md-offset-1 feed" style="margin-top:15px;">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3466.023331174392!2d75.23381441474486!3d29.69010324217997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39114750cb3ebcf1%3A0xa664627d8187f282!2sOld+Cinema+Rd%2C+Sardulgarh%2C+Punjab+151507%2C+India!5e0!3m2!1sen!2sau!4v1566361593798!5m2!1sen!2sau" width="800" height="450" frameborder="0" style="border:0" allowfullscreen>
+			</iframe>
+			<div style="margin-top:15px; float:left; "><h3>Contact Us</h3><hr>
+			
+			<p><i class="fa fa-phone"></i> +919646322813</p>
+			<p><i class="fa fa-envelope"></i> info@ohmcinema.com</p></div>
 		</div>
+		
 		<div class="col-md-10 col-md-offset-1 feed" style=" margin-top:15px">
 			<form action="feed.php" method="post">
 			<div class="row"><h3>Send Us Your Message</h3></div>
@@ -25,7 +29,7 @@
 				<p><label>Your Email Address</label><input name="mail" type="email" placeholder="Your Email Address" required></p>
 			</div>
 
-			<div class="col-md-12"><p><label>Your FeeBack</label><textarea name="feedback" placeholder="Your FeeBack" required></textarea></p></div>
+			<div class="col-md-12"><p><label>Your Message</label><textarea name="feedback" placeholder="Your Message" required></textarea></p></div>
 			<div class="col-md-4">
 				<input type="submit" value="SEND" class="button">
 			</div>
@@ -40,10 +44,7 @@
 		</div>
 
 		<div class="clearfix"></div>
-		<div class="col-md-10 col-md-offset-1">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3466.023331174392!2d75.23381441474486!3d29.69010324217997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39114750cb3ebcf1%3A0xa664627d8187f282!2sOld+Cinema+Rd%2C+Sardulgarh%2C+Punjab+151507%2C+India!5e0!3m2!1sen!2sau!4v1566361593798!5m2!1sen!2sau" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-			
-		</div>
+		
 
 	</div>
 
