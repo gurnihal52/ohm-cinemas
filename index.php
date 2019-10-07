704219b446527111fecbdebb33779475035c176f
gurnihal52 <46015716+gurnihal52@users.noreply.github.com>
2019-10-07 17:42:11 +1000

    messenger added

--- index.php
+++ index.php
@@ -4,52 +4,89 @@
 	
 
 			<div class="col-md-12 banner" style="margin-top:0px;">
-				<!-- <div class="col-md-4 col-md-offset-8 detail" >
-						<h2>BIG HERO 6</h2>
-						<p><span>Genre   </span>: Animation, Action, Comedy</p>
-						<p><span>Release </span>: 7 November 2014</p>
-
-						<p>The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hamada, who team up with a group of friends to form a band of high-tech heroes.</p>
-
-						<a href="https://www.youtube.com/watch?v=z3biFxZIJOQ" target="_blank" class="button shade"><i class="fa fa-youtube-play fa-2x fa-fw"></i><span style="width:150px; margin-left:10px;display:inline-block">WATCH TRAILER</span></a>
-
-						<a href="#" class="button shade"><i class="fa fa-ticket fa-2x fa-fw"></i><span style="width:150px; margin-left:10px;	display:inline-block">BOOK TICKET</span></a>
-						
-				</div> -->
-				<!-- <img src="images/b2.jpg" width="100%" height="100%"> -->
+				
 				<?php include('slider.php'); ?>
 			</div>
+
 		  <div class="clearfix"></div>
-			<div class="col-md-12 list">
-				<div class="col-md-2"><img src="images/r1.jpg" ></div>
-				<div class="col-md-2"><img src="images/r2.jpg" ></div>
-				<div class="col-md-2"><img src="images/r3.jpg" ></div>
-				<div class="col-md-2"><img src="images/r4.jpg" ></div>
-				<div class="col-md-2"><img src="images/r5.jpg" ></div>
-				<div class="col-md-2"><img src="images/r6.jpg" ></div>
-			</div>
-
-				<div class="clearfix"></div>
-
-			<div class="col-md-6 mid-left">
+		  <div class="col-md-4 mid-left">
 				<p>Don't be late,</p>
 				<h1>Book your ticket now!</h1>
 				<p>Easy, simple & fast.</p>
 
+		  </div>
 
-				<a href="" class="button shade1"><i class="fa fa-ticket fa-2x fa-fw"></i> <span>BOOK TICKET</span></a>
-			</div>
-
-			<div class="col-md-6 mid-right">
-				<h2>Categories</h2>
-				<div class="col-md-6"><a href=""><div><p>Romantic</p></div></a></div>
-				<div class="col-md-6"><a href=""><div><p>Comedy</p></div></a></div>
-				<div class="clearfix"></div>
-				<div class="col-md-6"><a href=""><div><p>Action</p></div></a></div>
-				<div class="col-md-6"><a href=""><div><p>Anime</p></div></a></div>
-			</div>
-
+			<div class="col-md-8 list">
+				<?php
+                 
+                    $cmd="select * from movies ORDER BY mid DESC LIMIT 4";
+                    $result = mysqli_query($conn, $cmd);
+                    $sn=0;
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        $sn++;
+                ?>      
+					<a href="single.php?movie=<?php echo $row[0]; ?>"><div class="col-md-3 col-xs-6"><span><i class="fa fa-ticket"></i> Book Ticket</span><img src="posters/<?php echo $row[7]; ?>" ></div></a>
+				<?php 
+					}
+				?>
 			
+			</div>
+
+				<div class="clearfix"></div>
+
+			<div class="col-md-7 mid-right list">
+				<h2>Upcoming Movies</h2>
+				<div class="clearfix"></div>
+				<marquee behavior="alternate" scrollamount="2">
+			<?php
+                 
+                    $cmd="select * from upmovies ORDER BY mid DESC LIMIT 4";
+                    $result = mysqli_query($conn, $cmd);
+                    $sn=0;
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        $sn++;
+                ?>      
+					<a href="comingsoon.php"><div class="col-md-3 col-xs-3"><span><i class="fa fa-ticket"></i> VIEW ALL</span><img src="upcoming/<?php echo $row[5]; ?>" ></div></a>
+				<?php 
+					}
+				?>
+			</marquee>
+			</div>
+
+			<div class="col-md-5 mid-right">
+				<h2>Categories</h2>
+				<div class="col-md-6 col-xs-6"><a href="movies.php?cat=romance"><div><p>Romantic</p></div></a></div>
+				<div class="col-md-6 col-xs-6"><a href="movies.php?cat=comedy"><div><p>Comedy</p></div></a></div>
+				<div class="clearfix"></div>
+				<div class="col-md-6 col-xs-6"><a href="movies.php?cat=action"><div><p>Action</p></div></a></div>
+				<div class="col-md-6 col-xs-6"><a href="movies.php?cat=anime"><div><p>Anime</p></div></a></div>
+			</div>
+
+			<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="117915609613028">
+      </div>
 
 
 
